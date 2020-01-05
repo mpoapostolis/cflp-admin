29231f87089096fa721596572eb9bd34c9a9b1af
mpoapostolis <mpoapostolis@gmail.com>
2020-01-05 07:36:42 +0200

    Offer: Add discount to my  products

--- src/routes/Offers/NewOffer/Details.tsx
+++ src/routes/Offers/NewOffer/Details.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useCallback } from 'react';
+import React, { useContext, useCallback, useState } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import {
   Card,
@@ -7,16 +7,37 @@ import {
   Divider,
   Grid,
   TextField,
-  MenuItem
+  MenuItem,
+  CardActions,
+  Button,
+  Typography,
+  InputAdornment,
+  Hidden,
+  IconButton
 } from '@material-ui/core';
 import I18n from '../../../I18n';
 import { useSelector } from 'react-redux';
 import { IReduxStore } from '../../../redux/reducers';
+import DeleteIcon from '@material-ui/icons/Delete';
 import * as R from 'ramda';
+import AsyncAutoComplete from './AsyncAutoComplete';
+import { red } from '@material-ui/core/colors';
+
+const randomString = () =>
+  Math.random()
+    .toString(36)
+    .substring(2, 15) +
+  Math.random()
+    .toString(36)
+    .substring(2, 15);
 
 const useStyles = makeStyles(() => ({
   cardContent: {
     minHeight: '167px'
+  },
+  btn: {
+    display: 'flex',
+    justifyContent: 'space-arround'
   },
 
   spacer: {
@@ -39,16 +60,61 @@ type Props = {
   >;
 };
 
+type Discount = {
+  name?: string;
+  price?: number;
+  discount?: number;
+  tableId: string;
+};
+
 function AccountDetails(props: Props) {
   const classes = useStyles();
   const { infos, setInfos } = props;
+  const [discounts, setDiscounts] = useState<Discount[]>([]);
   const errors = useSelector((store: IReduxStore) => store.errors);
+
+  function addDiscount() {
+    setDiscounts(s => [...s, { tableId: randomString() }]);
+  }
+
+  const chooseProduct = useCallback(
+    (obj: Partial<Discount>, idx: number) => {
+      const infos = {
+        ...R.pick(['name', 'price', '_id'], { ...obj }),
+        discount: 0,
+        tableId: discounts[idx].tableId
+      };
+      setDiscounts(s => s.map((o, i) => (i === idx ? infos : o)));
+    },
+    [discounts]
+  );
+
+  const setProductDiscount = useCallback(
+    (discount: number, idx: number) => {
+      const tmp = R.assocPath(
+        [idx, 'discount'],
+        Math.max(Math.min(discount, 100), 0),
+        discounts
+      );
+      setDiscounts(tmp);
+    },
+    [discounts]
+  );
+
+  const deleteDiscount = (idx: number) => {
+    setDiscounts(s => s.filter((_, i) => i !== idx));
+  };
 
   const _setInfos = useCallback(obj => {
     setInfos(s => ({ ...s, ...obj }));
   }, []);
 
   const t = useContext(I18n);
+
+  function finalPrice(discount: number = 0, price?: number) {
+    if (!price) return '';
+    return `${(price * ((100 - discount) / 100)).toFixed(2)} €`;
+  }
 
   return (
     <Card>
@@ -57,7 +123,7 @@ function AccountDetails(props: Props) {
         title={t('int.product')}></CardHeader>
       <Divider />
       <CardContent className={classes.cardContent}>
-        <Grid container spacing={2}>
+        <Grid container alignItems="center" spacing={2}>
           <Grid item md={12} xs={12}>
             <TextField
               fullWidth
@@ -110,8 +176,96 @@ function AccountDetails(props: Props) {
               ))}
             </TextField>
           </Grid>
+
+          {discounts.length > 0 && (
+            <Grid item md={12} xs={12}>
+              <br />
+              <Typography variant={'h5'}>{t('int.discounts')}</Typography>
+              <br />
+            </Grid>
+          )}
+
+          {discounts.map((obj, idx) => (
+            <React.Fragment key={obj.tableId}>
+              <Grid item md={10} xs={10}>
+                <Typography variant="caption">
+                  {t('int.discount')}: {idx + 1}
+                </Typography>
+                <Divider />
+              </Grid>
+              <Grid item md={4} xs={9}>
+                <AsyncAutoComplete
+                  url={'/api/bo/products'}
+                  onChange={obj => chooseProduct(obj, idx)}
+                />
+              </Grid>
+              <Hidden smDown>x</Hidden>
+              <Grid item md={4} xs={6}>
+                <TextField
+                  fullWidth
+                  onChange={evt =>
+                    setProductDiscount(+evt.currentTarget.value, idx)
+                  }
+                  disabled={Boolean(!discounts[idx].name)}
+                  margin={'dense'}
+                  label={t('int.discount')}
+                  type={'number'}
+                  value={discounts[idx].discount || ''}
+                  inputProps={{
+                    min: 0,
+                    max: 100
+                  }}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="start">%</InputAdornment>
+                    )
+                  }}
+                  variant="outlined"
+                />
+              </Grid>
+              =
+              <Grid item md={2} xs={2}>
+                <Typography
+                  style={{
+                    textDecoration: discounts[idx].discount
+                      ? 'line-through'
+                      : '',
+                    opacity: discounts[idx].discount ? '0.5' : ''
+                  }}>
+                  {`${obj?.price}€` ?? ''}
+                </Typography>
+                {discounts[idx].discount ? (
+                  <Typography>
+                    {finalPrice(discounts[idx].discount, obj.price)}
+                  </Typography>
+                ) : (
+                  ''
+                )}
+              </Grid>
+              <Grid item md={1} xs={1}>
+                <IconButton
+                  onClick={() => deleteDiscount(idx)}
+                  size={'small'}
+                  title={t('int.delete')}>
+                  <DeleteIcon htmlColor={red[500]} />
+                </IconButton>
+              </Grid>
+            </React.Fragment>
+          ))}
         </Grid>
       </CardContent>
+      <Divider />
+      <CardActions>
+        <span className={classes.spacer}></span>
+
+        <Button
+          onClick={addDiscount}
+          className={classes.btn}
+          title={t('int.add-discount')}
+          variant={'outlined'}>
+          {t('int.add-discount')}
+        </Button>
+      </CardActions>
     </Card>
   );
 }
